3cf9bfc8b4e051c7c5100047c9376783439c109d
shalvamist <shalvamm@gmail.com>
2024-03-17 09:08:04 -0700

    Updated the LlamaCPP install, looks lke it's fixed

--- setup.sh
+++ setup.sh
@@ -5,17 +5,18 @@
 pip install -r requirements.txt
 
 ## Setting up LlamaCPP
-git clone https://github.com/ggerganov/llama.cpp
-export LLAMA_CUBLAS=on
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install llama-cpp-python
+pip install huggingface_hub
 
-cd llama.cpp
-make clean
-make libllama.so
-
-export LLAMA_CPP_LIB=./llama.cpp/libllama.so
-cd ..
-
-CMAKE_ARGS="-DLLAMA_CUBLAS=on -DCUDA_PATH=/usr/local/cuda-12.2 -DCUDAToolkit_ROOT=/usr/local/cuda-12.2 -DCUDAToolkit_INCLUDE_DIR=/usr/local/cuda-12/include -DCUDAToolkit_LIBRARY_DIR=/usr/local/cuda-12.2/lib64 -DCMAKE_CUDA_COMPILER:PATH=/usr/local/cuda/bin/nvcc" FORCE_CMAKE=1 pip install --upgrade --force-reinstall llama-cpp-python --no-cache-dir --verbos
+## In case the above installation doesn't work for LlamaCPP - use this
+# git clone https://github.com/ggerganov/llama.cpp
+# export LLAMA_CUBLAS=on
+# cd llama.cpp
+# make clean
+# make libllama.so
+# export LLAMA_CPP_LIB=./llama.cpp/libllama.so
+# cd ..
+# CMAKE_ARGS="-DLLAMA_CUBLAS=on -DCUDA_PATH=/usr/local/cuda-12.2 -DCUDAToolkit_ROOT=/usr/local/cuda-12.2 -DCUDAToolkit_INCLUDE_DIR=/usr/local/cuda-12/include -DCUDAToolkit_LIBRARY_DIR=/usr/local/cuda-12.2/lib64 -DCMAKE_CUDA_COMPILER:PATH=/usr/local/cuda/bin/nvcc" FORCE_CMAKE=1 pip install --upgrade --force-reinstall llama-cpp-python --no-cache-dir --verbos
 
 ## Setting up Ollama
 curl -fsSL https://ollama.com/install.sh | sh
